018fdf64c12c3ec53476ddc674f9794dbfdda9c6
chaun14 <chaun1403@gmail.com>
2022-01-11 19:16:10 +0100

    Add default public api page

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -4,6 +4,12 @@ import invites from "./invites";
 
 const v1: Router = Router();
 
+v1.get("/", (req, res) => {
+  res.send({
+    message: "Welcome to inviteLogger api. Find our documentation here: https://developers.invitelogger.me",
+  });
+});
+
 v1.use("/", dummy);
 v1.use("/invites/", invites);
 
