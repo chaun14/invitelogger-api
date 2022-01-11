87912772625e59cc0d6ab656731cd02191632a3f
chaun14 <chaun1403@gmail.com>
2022-01-11 19:20:32 +0100

    Move auth middleware to public router

--- src/routes/dummy/index.ts
+++ src/routes/dummy/index.ts
@@ -1,10 +1,8 @@
-import tokenAuth from "../../middlewares/tokenAuth";
 import express from "express";
 import controller from "../../controllers/dummy";
 
 const router = express.Router();
 
-router.use(tokenAuth);
 router.get("/dummy", controller.handleDummy);
 
 export = router;
--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -1,15 +1,21 @@
+import tokenAuth from "../middlewares/tokenAuth";
 import { Router } from "express";
 import dummy from "./dummy";
 import invites from "./invites";
 
 const v1: Router = Router();
 
+// public api main page
 v1.get("/", (req, res) => {
   res.send({
     message: "Welcome to inviteLogger api. Find our documentation here: https://developers.invitelogger.me",
   });
 });
 
+// authenticate requests
+v1.use(tokenAuth);
+
+// public router
 v1.use("/", dummy);
 v1.use("/invites/", invites);
 
