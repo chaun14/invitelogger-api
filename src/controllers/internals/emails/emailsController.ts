5133961006e78de7caca1b666b8dbe84080f9df0
Enes Tahiri <125091714+e-enes@users.noreply.github.com>
2024-04-21 20:17:27 +0200

    refactor: added email controller

--- src/controllers/internals/emails/emailsController.ts
+++ src/controllers/internals/emails/emailsController.ts
@@ -1,5 +1,35 @@
 import { Request, Response } from "express";
 
-export const handleEmail = async (_req: Request, res: Response) => {
-  res.status(200).json({});
+import { sendEmail } from "@utils/email";
+
+type EmailBody = {
+  email: string;
+  subject: string;
+  message: string;
+  options?: {
+    title?: string;
+    button_url?: string;
+    button_txt?: string;
+  };
+};
+
+export const handleEmail = async (req: Request, res: Response) => {
+  const { body }: { body?: EmailBody } = req;
+
+  if (!body || !body?.email || !body?.subject || !body?.message) {
+    res.status(400).json({ message: "Missing parameter" });
+    return;
+  }
+
+  await sendEmail(body.email, {
+    subject: body.subject,
+    message: {
+      title: body.options?.title,
+      content: body.message,
+      buttonText: body.options?.button_txt || "Go to the gold dashboard",
+      buttonUrl: body.options?.button_url || "https://gold.invitelogger.me",
+    },
+  });
+
+  res.status(200).send({ message: "Email sent" });
 };
